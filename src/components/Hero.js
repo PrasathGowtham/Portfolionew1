de8cf5a58bc7ddf16c86138a97c7c8f5b00e13e3
Gowtham Prasath <108874931+PrasathGowtham@users.noreply.github.com>
2025-05-11 16:17:56 +0530

    Add files via upload

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from 'react';
-import Lottie from 'lottie-web';
-import animate from 'lottie-web';
+import React, { useEffect, useRef } from "react";
+import Lottie from "lottie-web";
+import animate from "lottie-web";
 
 const Hero = () => {
   const container = useRef(null);
@@ -8,23 +8,33 @@ const Hero = () => {
   useEffect(() => {
     Lottie.loadAnimation({
       container: container.current,
-      renderer: 'svg',
+      renderer: "svg",
       loop: true,
       autoplay: true,
-      animationData: require('./astronaout.json'),
+      animationData: require("./astronaout.json"),
     });
     animate.setSpeed(0.5);
   }, []);
 
   return (
-    <section className='my-16 px-5 md:my-32 flex flex-col flex-wrap space-y-16 md:space-y-0 patterns'>
-      <div className='ml-auto w-72 md:w-96 lg:w-2/5'>
-        <div className='container' ref={container}></div>
+    <section className="my-16 px-5 md:my-32 flex flex-col md:flex-row items-center justify-between space-y-16 md:space-y-0 patterns">
+      {/* Image or container on the right */}
+      <div className="w-72 md:w-96 lg:w-2/5">
+        <div className="container" ref={container}></div>
       </div>
-      <div className='max-w-md mb-auto space-y-5'>
-        <h1 className='text-5xl font-bold md:text-7xl'>Hello, I am Gowtham Prasath</h1>
-        <p className='tracking-wide leading-relaxed'>
-        About a years ago I decided I wanted more from my career - plus I really love learning, So I decided to teach myself how to code and I started with HTML5 and CSS3 for Better knowladge I moved to Full Stack devolepment Course (MERN) in Guvi Geek Institue from june 2022 to oct 2022 before my Full Stack Course I had complited B.E Civil Engineering in year 2018, and  worked as site Engineer in Apexinfralink limited till May 2022.      </p>
+      {/* Text on the left */}
+      <div className="max-w-md space-y-5 md:mr-10">
+        <h1 className="text-5xl font-bold md:text-6xl">
+          Hello, I am Gowtham Prasath
+        </h1>
+        <p className="tracking-wide leading-relaxed">
+          As a Software Developer with 2+ years of experience, I specialize in
+          building and maintaining scalable web applications using React.js,
+          Node.js, PostgreSQL, Mongodb. I focus on creating intuitive
+          interfaces, optimizing databases, and developing robust RESTful APIs.
+          A strong team player with a passion for leveraging technology to drive
+          operational efficiency and deliver high-quality solutions.
+        </p>
       </div>
     </section>
   );
